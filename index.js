eed51d0e52608b0ec191c9de25dd551fbd5b5024
Joshua Jones <joshuajones@trentu.ca>
2020-01-26 15:58:42 -0500

    added cors middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
+const cors = require('cors')
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
 
@@ -11,6 +12,9 @@ const router = require('./router')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+app.use(router)
+app.use(cors())
 
 io.on('connection', (socket) => {
 	// 'join' is the keyword that we passed from the client, here when 'join' is presented we run a function
@@ -46,7 +50,5 @@ io.on('connection', (socket) => {
 		}
 	})
 })
-
-app.use(router)
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
